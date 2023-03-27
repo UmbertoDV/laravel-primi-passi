0bb9d4f1fd505193057bfb5df1d8ed52b65e2cc2
UmbertoDV <biccoumby2002@hotmail.com>
2023-03-27 16:42:59 +0200

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="flex justify-center items-center h-screen text-3xl flex-col">
+        <h1 class="font-semibold text-4xl">Hello World!</h1>
+        <h2>Con <span class="font-semibold underline">Tailwind CSS</span></h2>
+        <img src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="">
+    </div>
+</body>
+</html>
